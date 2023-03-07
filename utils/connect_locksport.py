b85c6339724b6479177399366bb72fc51814efa5
kaa-serpent <guillaumedecadoudal@gmail.com>
2023-03-07 17:00:58 +0100

    separated and clean

--- utils/connect_locksport.py
+++ utils/connect_locksport.py
@@ -18,6 +18,25 @@ def parse_research(driver: object):
     return driver, prettify_data
 
 
+def research(driver: object, str_to_research: str):
+    """Function that research on locksport.fr"""
+    driver.get("https://www.locksport.fr/search.php")
+
+    terme_search = driver.find_element(By.ID, 'keywords')
+    terme_title_only = driver.find_element(By.ID, 'sf3')
+    terme_search.send_keys(str_to_research)
+    terme_title_only.click()
+
+    submit_button = driver.find_element(By.CLASS_NAME, "button1")
+
+    # scroll to the submit button using JavaScript
+    driver.execute_script("arguments[0].scrollIntoView();", submit_button)
+    submit_button.click()
+
+    WebDriverWait(driver, 10)
+    return driver
+
+
 def login(message=None):
 
     with open('credentials.json') as f:
@@ -27,12 +46,17 @@ def login(message=None):
 
     # set up a Firefox webdriver with headless mode enabled
     firefox_options = Options()
-    firefox_options.headless = True
+    firefox_options.headless = False
     driver = webdriver.Firefox(options=firefox_options)
 
     # navigate to the login page
     login_url = 'https://www.locksport.fr/ucp.php?mode=login'
     driver.get(login_url)
+
+    # accept cookies
+    cookies_button = driver.find_element(By.CLASS_NAME, 'cc-btn cc-dismiss')
+    cookies_button.click()
+    WebDriverWait(driver, 10)
 
     # fill in the login form and submit it
     username_input = driver.find_element(By.ID, 'username')
@@ -47,32 +71,17 @@ def login(message=None):
     # wait for the login process to complete
     WebDriverWait(driver, 10)
 
-    driver.get("https://www.locksport.fr/search.php")
-
-    terme_search = driver.find_element(By.ID, 'keywords')
-    terme_title_only = driver.find_element(By.ID, 'sf3')
-    terme_search.send_keys(message)
-    terme_title_only.click()
-
-    submit_button = driver.find_element(By.CLASS_NAME, "button1")
-
-    # scroll to the submit button using JavaScript
-    driver.execute_script("arguments[0].scrollIntoView();", submit_button)
-    submit_button.click()
-
-    WebDriverWait(driver, 10)
-    # close the webdriver when done
-
-    driver, prettify_data = parse_research(driver)
-
-    return driver, prettify_data
+    return driver
 
 
 def search(message=None):
     """Function that searches for a specific lock on Locksport.fr"""
     if message == '':
-        return ["Pas de mot a rechercher fournis"]
-    driver, prettify_data = login(message)
+        return "Pas de mot(s) a rechercher fourni(s)"
+    driver = login(message)
+    driver = research(driver, message)
+    prettify_data = parse_research(driver)
+
     print(prettify_data)
     driver.quit()
     return prettify_data
